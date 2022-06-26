1cc805091b6dcee6b158153ee9326f42824dd50e
Guru Karthikeyan <gurukartkn@gmail.com>
2022-06-26 19:29:43 +0530

    feat: setup apollo client for ssr

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from 'next';
+import type { GetServerSidePropsContext, NextPage } from 'next';
 import Head from 'next/head';
 import { RegisterForm } from '../components/Register/Form';
 import { Navbar } from '../components/Register/Navbar';
+import { addApolloState, initializeApollo } from '../lib/apolloClient';
+import { prisma } from '../lib/prisma';
 
 const Register: NextPage = () => {
   return (
@@ -9,10 +11,21 @@ const Register: NextPage = () => {
       <Head>
         <title>Register</title>
       </Head>
+
       <Navbar />
       <RegisterForm />
     </div>
   );
+};
+
+export const getServerSideProps = async ({
+  req,
+}: GetServerSidePropsContext) => {
+  const apolloClient = initializeApollo({ ctx: { req, prisma } });
+
+  return addApolloState(apolloClient, {
+    props: {},
+  });
 };
 
 export default Register;
