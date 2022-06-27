8a3d974366250980ce385a13be9ff106a844db25
Guru Karthikeyan <gurukartkn@gmail.com>
2022-06-27 10:37:12 +0530

    build: configure graphql-codegen

--- modules/graphql/types.ts
+++ modules/graphql/types.ts
@@ -6,13 +6,6 @@ export const testQuery = extendType({
     t.boolean('test', {
       args: { bool: nonNull(booleanArg()) },
       resolve: async (_, { bool }, { prisma }) => {
-        await prisma.user.create({
-          data: {
-            email: 'abc@gmail.com',
-            username: 'abc',
-            passhash: 'abc',
-          },
-        });
         const users = await prisma.user.findMany();
         console.log(users);
         return bool;
--- pages/register.tsx
+++ pages/register.tsx
@@ -2,15 +2,22 @@ import type { GetServerSidePropsContext, NextPage } from 'next';
 import Head from 'next/head';
 import { RegisterForm } from '../components/Register/Form';
 import { Navbar } from '../components/Register/Navbar';
+import { TestDocument, useTestQuery } from '../generated/graphql';
 import { addApolloState, initializeApollo } from '../lib/apolloClient';
 import { prisma } from '../lib/prisma';
 
 const Register: NextPage = () => {
+  const { data } = useTestQuery({
+    notifyOnNetworkStatusChange: true,
+  });
+
   return (
     <div className="bg-violet-100 min-h-screen">
       <Head>
         <title>Register</title>
       </Head>
+
+      <div>{JSON.stringify(data?.test)}</div>
 
       <Navbar />
       <RegisterForm />
@@ -22,6 +29,8 @@ export const getServerSideProps = async ({
   req,
 }: GetServerSidePropsContext) => {
   const apolloClient = initializeApollo({ ctx: { req, prisma } });
+
+  await apolloClient.query({ query: TestDocument });
 
   return addApolloState(apolloClient, {
     props: {},
